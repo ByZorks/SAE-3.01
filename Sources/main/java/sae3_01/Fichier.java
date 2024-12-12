f105a8be8ee4ac34b51828438ca0e8a5c4bca0f9
David <80073731+ByZorks@users.noreply.github.com>
2024-12-12 09:28:51 +0100

    indentation composite

--- Sources/main/java/sae3_01/Fichier.java
+++ Sources/main/java/sae3_01/Fichier.java
@@ -1,6 +1,9 @@
 package sae3_01;
+
 import java.io.File;
+
 public class Fichier extends FileComposite{
+
     public Fichier(File file) {
         super(file);
     }
--- Sources/main/java/sae3_01/FileComposite.java
+++ Sources/main/java/sae3_01/FileComposite.java
@@ -3,11 +3,11 @@ package sae3_01;
 import java.io.File;
 
 public abstract class FileComposite {
+
     protected File file;
-     public FileComposite(File file) {
-         this.file = file;
-     }
-      // public abstract void afficher(String s){
 
-     }
+    public FileComposite(File file) {
+        this.file = file;
+    }
 
+}
